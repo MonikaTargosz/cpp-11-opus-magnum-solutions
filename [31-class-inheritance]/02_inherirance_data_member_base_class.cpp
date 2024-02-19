259471aa112f2019180b444e6f9e0d8d7f454f00
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-19 22:26:48 +0100

    Chapter 31. S2. code
    - inheritance data member of base class

---
+++ [31-class-inheritance]/02_inherirance_data_member_base_class.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+/**
+ * @brief Class B inherits class A, in which there is a (non-private) given-component x. 
+ * In class B, there is only a member function f. 
+ * To use the data member x you need to 
+ */
+
+/**
+ * @brief Base class with non-private data member x
+ */
+
+class A 
+{
+    protected:
+        int x;
+    public:
+
+        /**
+         * @brief Function to set the private data member
+         * 
+         * @param m 
+         */
+        void set(int m) { x = m; }
+
+        /**
+         * @brief Function get the private data member
+         * 
+         * @return int 
+         */
+        int get() { return x; }
+};
+
+/**
+ * @brief Class B inherits class A with member function f 
+ * to set the non-private data member of class A
+ */
+
+class B : public A
+{
+    public:
+        void f();   
+};
+
+
+void B::f()
+{
+    set(5);
+    std::cout<<get();
+}
+
+int main()
+{
+    B b;
+    b.f();
+}
