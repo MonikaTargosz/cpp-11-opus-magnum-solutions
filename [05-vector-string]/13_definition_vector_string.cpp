36d26c27da5cb9443dec1630a10b959246f6a7cd
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-21 21:45:57 +0100

    Chapert 5. S13. Definitions of string type obiect
    - Rename file S12.

---
+++ [05-vector-string]/13_definition_vector_string.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file 13_definition_vector_string.cpp
+ * @brief Definition of a vector named k in which the names of several colleagues can be stored.
+ * @version 0.1
+ * @date 2024-02-21
+ */
+
+ #include <iostream>
+ #include <vector>
+ #include <string>
+
+ int main()
+ {
+	std::vector<std::string> k { "Kowalski", "Kwiatkowski", "Nowak" };
+ }
