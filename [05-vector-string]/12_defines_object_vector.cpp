35914d1ade987739da5ed13c575938125ea3d6c5
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-21 21:40:17 +0100

    Chapter 5. S12. Defines an object of type std::vector<int> and immediately initialises

---
+++ [05-vector-string]/12_defines_object_vector.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file 12_defines_object_vector.cpp
+ * @brief Defines an object of type std::vector<int> named v and immediately initialises it with the values 30, 40, 50.
+ * @version 0.1
+ * @date 2024-02-21
+ * 
+ */
+
+ #include <iostream>
+ #include <vector>
+
+ int main()
+ {
+	std::vector<int> v { 30, 40, 50 };
+ }
