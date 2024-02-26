67eaf0bd41c03760ca159b9a1389be87607addb0
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-26 21:28:35 +0100

    Chapter 20. S05. Definition of a member function of class W nested within class A.

--- [20-member_class]/03-12-using-nested_class.cpp
+++ [20-member_class]/03-12-using-nested_class.cpp
@@ -9,22 +9,7 @@ class A
         int m_priv_W;
         public:
     
-            void set(int l_on)
-            {
-                A o_A;
-                
-                /* S03. The member functions of class W can use the private components of class A. */
-                if(l_on)
-                {
-                    m_priv_W = o_A.m_priv_A = 1;
-                    std::cout<<"light on\n";
-                }
-                else
-                {
-                    m_priv_W = o_A.m_priv_A = 0;
-                    std::cout<<"light off\n";
-                }
-            }
+            void set(int l_on);
     };
     
     W oWew;
@@ -35,6 +20,25 @@ class A
         
     
 };
+
+/* S05. Definition of a member function of class W nested within class A. */
+
+void A::W::set(int l_on)
+{ 
+    A o_A;
+    
+    /* S03. The member functions of class W can use the private components of class A. */
+    if(l_on)
+    {
+        m_priv_W = o_A.m_priv_A = 1;
+        std::cout<<"light on\n";
+    }
+    else
+    {
+        m_priv_W = o_A.m_priv_A = 0;
+        std::cout<<"light off\n";
+    }
+}
 
 void A::set(int l_on)
 {
