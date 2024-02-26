3dc5aaf47362a5e896ac5561da789ce8dc431843
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-26 22:04:50 +0100

    Chapter 20. S07. Possible definition of a member function of class W nested within class A. S08. Definition of a static data-member of class W nested within class A - must be outside the class body. S12. Class A cannot use the enum type of the nested class W.

--- [20-member_class]/03-12-using-nested_class.cpp
+++ [20-member_class]/03-12-using-nested_class.cpp
@@ -4,11 +4,23 @@ class A
 {
     int m_priv_A;
     
+    public:
+        
+        enum delay_a{one,two,three,four,five};
+    
+    private:
+
     class W
     {
         int m_priv_W;
+        
+        static int interval;
+
         public:
+        
+            enum delay_e{one,two,three,four,five};
     
+            /* S05. & S07 Possible definition of a member function of class W nested within class A. */
             void set(int l_on);
     };
     
@@ -21,11 +33,20 @@ class A
     
 };
 
-/* S05. Definition of a member function of class W nested within class A. */
+/* S05. & S07 Definition of a member function of class W nested within class A. */
 
 void A::W::set(int l_on)
 { 
     A o_A;
+    
+    
+    /* S12. Class A cannot use the enum type of the nested class W. */
+    //delay_a delay_in_minutes;
+    //delay_in_minutes = two;
+    
+    delay_e delay_in_seconds;
+    delay_in_seconds = one;
+
     
     /* S03. The member functions of class W can use the private components of class A. */
     if(l_on)
@@ -39,6 +60,9 @@ void A::W::set(int l_on)
         std::cout<<"light off\n";
     }
 }
+
+/* S08. Definition of a static data-member of class W nested within class A - must be outside the class body.*/
+int A::W::interval = 100;
 
 void A::set(int l_on)
 {
