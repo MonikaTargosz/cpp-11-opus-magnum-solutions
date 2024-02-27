c9e14b0d7877c1331b0855ce2eb70373b3eca08d
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-27 18:32:06 +0100

    Chapter 23. S01. Array of obiects of the class

---
+++ [23-array-of-objects]/01-array-pcb-of-obiects-K.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file 01-array-pcb-of-obiects-K.cpp
+ * @brief The array pcb of obects of the class K.
+ * @version 0.1
+ * @date 2024-02-27
+ */
+#include <iostream>
+
+/**
+ * @brief In class K there is a public component t
+ * hat is a 70-element array of type double.
+ */
+
+class K				// class – simple aggregate  	
+{
+public:
+    double		capacitor[70];
+};
+
+int main()
+{
+
+    /**
+     * @brief A 20-element array of objects of this class K is defined. 
+     */
+
+    K  pcb [20] = 										 
+    {
+        K{ {2.4, 3.3} },			// data for pcb[0]
+        K{ },				        // data for pcb[1] 
+        K{ {2.4, 3.3, 33.0} },				
+        K{  },							
+        K{ {2.4, 3.3, 33.0} },      // data for pcb[4]
+        { }										
+    };
+
+    for(unsigned int i = 0; i < 20 ; ++i) 
+    {
+        if(pcb[i].capacitor[0]) 	// is there anything in the capacitor array?
+        {
+            std::cout << "PCB no. " << i << std::endl;
+            std::cout << "\t:Capacitor no. 2: " << pcb[i].capacitor[2] << " nF" << std::endl;
+        }
+    }
+    
+        /* S01. The expression relating to the capacitor with index 66 located in a pcb with index 4. */
+        std::cout << "PCB no. 4 has capacitor no. 66: " << pcb[4].capacitor[66] << " nF" << std::endl;
+
+}
