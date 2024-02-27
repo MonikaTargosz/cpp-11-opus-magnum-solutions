93e0a011f8f9c9362dfd5457d56355e8b05d7cbf
Monika Szafraniec <monikatargosz96@gmail.com>
2024-02-27 19:23:04 +0100

    Chapter 23. S03. Const object definition
    The initial value can only be assigned during const object definition using the initialiser list.
    
    - changing the beginning of a file name from 01 to 01-04

--- [23-array-of-objects]/01-04-array-pcb-of-obiects-K.cpp
+++ [23-array-of-objects]/01-04-array-pcb-of-obiects-K.cpp
@@ -20,6 +20,7 @@ class K				// class – simple aggregate
 */
 
 public:
+    const char  version[20];
     double		capacitor[70];
 };
 
@@ -29,16 +30,18 @@ int main()
     /**
      * @brief A 20-element array of objects of this class K is defined. 
      */
+     
+    const K myPcb = {"empty", {0, 0, 0, 0 } };
 
     constexpr int pcb_amount = 20;
 
     K  pcb [pcb_amount] = 										 
     {
-        K{ {2.4, 3.3} },			// data for pcb[0]
-        K{ },				        // data for pcb[1] 
-        K{ {2.4, 3.3, 33.0} },				
-        K{  },							
-        K{ {2.4, 3.3, 33.0} },      // data for pcb[4]
+        K{ "A", {2.4, 3.3} },			// data for pcb[0]
+        K{ },				            // data for pcb[1] 
+        K{ "C", {2.4, 3.3, 33.0} },				
+        K{ "D",},							
+        K{ "E", {2.4, 3.3, 33.0} },      // data for pcb[4]
         { }							
         /* S04. Brackets separating the expressions initialising 
         the individual elements of the array are not necessary.*/			
@@ -48,12 +51,17 @@ int main()
     {
         if(pcb[i].capacitor[0]) 	// is there anything in the capacitor array?
         {
-            std::cout << "PCB no. " << i << std::endl;
-            std::cout << "\t:Capacitor no. 2: " << pcb[i].capacitor[2] << " nF" << std::endl;
+            std::cout << "PCB ver. " << pcb[i].version << std::endl;
+            std::cout << "\tCapacitor no. 2: " << pcb[i].capacitor[2] << " nF" << std::endl;
         }
     }
     
         /* S01. The expression relating to the capacitor with index 66 located in a pcb with index 4. */
         std::cout << "PCB no. 4 has capacitor no. 66: " << pcb[4].capacitor[66] << " nF" << std::endl;
+        
+        /* S03. The initial value can only be assigned during object definition using the initialiser list. */
+        
+        //myPcb.version = "Template";
+        //pcb[4].version = "F";   // error: assignment of read-only member ‘K::version’
 
 }
